f8b46d47e0f36ea4b03ad190d820ee9d7003487e
jpg <sangong10@gmail.com>
2022-11-03 13:43:48 +0900

    create files for finder

---
+++ src/finder/finder.h
@@ -0,0 +1,37 @@
+#ifndef _DB_H
+#define _DB_H
+#include "array.h"
+#include "profile.h"
+#define DATA_COUNT "DATA_COUNT"
+#define FILE_TITLE "FILE_TITLE"
+#define FILE_DATA "FILE_DATA"
+#define FILE_PATH "FILE_PATH"
+#define FILE_TAGS "FILE_TAGS"
+
+typedef struct{
+    char *title;
+    char *data;
+    char *file;
+    LPARRAY tags;
+} Data;
+typedef struct DB{
+    struct DB *m_this; // 자신을 가리키는 포인터 변수
+    LPHASH m_lpHashTag;
+    Data *m_lpData; // {char *title, char *data, char *file, LPARRAY tags;}    
+    int m_nData;
+
+    void (*deleteDB)(struct DB *); // 소멸자
+    void (*save)(struct DB *, Data *);
+    void (*remove)(struct DB *, char *);
+    void (*getTag)(struct DB *);
+    int (*load)(struct DB *);
+}DB;
+
+DB *newDB();
+void deleteDB_(DB *); // 소멸자
+void save_(DB *, Data *);
+void remove_(DB *, char *);
+void getTag_(DB *);
+int load_(DB *);
+
+#endif
